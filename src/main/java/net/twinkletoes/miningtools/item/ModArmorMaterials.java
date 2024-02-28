1bf4fdb89a0fbc32abd372785232a5fc5fb0ec85
Nayma Garcia <90876995+Nayma-Garcia@users.noreply.github.com>
2024-02-28 17:45:43 -0600

    added empty armor functions

--- src/main/java/net/twinkletoes/miningtools/item/ModArmorMaterials.java
+++ src/main/java/net/twinkletoes/miningtools/item/ModArmorMaterials.java
@@ -1,4 +1,50 @@
 package net.twinkletoes.miningtools.item;
 
-public enum ModArmorMaterials {
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+    ;
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return 0;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 0;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return null;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
 }
