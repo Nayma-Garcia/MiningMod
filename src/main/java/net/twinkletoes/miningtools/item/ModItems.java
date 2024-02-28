967e87c03d703d71eff5d316a79b89296812e267
Nayma Garcia <90876995+Nayma-Garcia@users.noreply.github.com>
2024-02-28 13:11:36 -0600

    setting up new item

---
+++ src/main/java/net/twinkletoes/miningtools/item/ModItems.java
@@ -0,0 +1,11 @@
+package net.twinkletoes.miningtools.item;
+
+import net.twinkletoes.miningtools.Miningtools;
+
+public class ModItems {
+
+    public static void registerModItems(){
+        Miningtools.LOGGER.info("Registering Mod Items for " + Miningtools.MOD_ID);
+    }
+
+}
