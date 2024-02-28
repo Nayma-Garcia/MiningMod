dbdc69bf4ac7eb11f60c1bdddb1be460b1e24dfe
Nayma Garcia <90876995+Nayma-Garcia@users.noreply.github.com>
2024-02-28 11:12:41 -0600

    first commmit

---
+++ src/main/java/net/twinkletoes/miningtools/MiningtoolsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.twinkletoes.miningtools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MiningtoolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
