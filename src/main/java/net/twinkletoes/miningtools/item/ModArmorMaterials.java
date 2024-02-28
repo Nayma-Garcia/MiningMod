10c8cc523781e933ed0a5d6581ea5bf7f0cbd8d9
Nayma Garcia <90876995+Nayma-Garcia@users.noreply.github.com>
2024-02-28 17:50:53 -0600

    added constructor

--- src/main/java/net/twinkletoes/miningtools/item/ModArmorMaterials.java
+++ src/main/java/net/twinkletoes/miningtools/item/ModArmorMaterials.java
@@ -21,6 +21,17 @@ public enum ModArmorMaterials implements ArmorMaterial {
 
     private static final int[] BASE_DURABILITY = {11, 16, 15, 13};
 
+    ModArmorMaterials(String name, int durabilityMultiplier, int[] protectionAmounts, int[] enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantability = enchantability;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = repairIngredient;
+    }
+
     @Override
     public int getDurability(ArmorItem.Type type) {
         return 0;
